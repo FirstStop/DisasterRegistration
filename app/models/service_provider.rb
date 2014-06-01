2214c1fce86b44d6210a98e85c5e7a81057d763c
Nikita Rybak <nikita.rybak@gmail.com>
2014-06-01 13:16:59 +1000

    unused message

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -1,2 +1,3 @@
 class ServiceProvider < ActiveRecord::Base
+  has_and_belongs_to_many :clients, :class_name => "Person", :join_table => "people_providers"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601012237) do
+ActiveRecord::Schema.define(version: 20140601031212) do
 
   create_table "people", force: true do |t|
     t.string   "first_name"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 20140601012237) do
   end
 
   add_index "people", ["uuid"], name: "index_people_on_uuid"
+
+  create_table "people_providers", force: true do |t|
+    t.integer "person_id"
+    t.integer "service_provider_id"
+  end
 
   create_table "service_providers", force: true do |t|
     t.string   "name"
