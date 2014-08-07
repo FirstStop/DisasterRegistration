06b80ce018b88c2af3ffb64596889df9887e4f91
Andreas Thurn <AndreasThurn@gmx.de>
2014-08-07 17:35:02 +1000

    start working on acls

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,6 +7,16 @@ class Person < ActiveRecord::Base
     "#{last_name}, #{first_name}"
   end
 
+  def accessible_for_user authenticable
+    if authenticable.try(:authenticable_object).nil?
+      true
+    elsif authenticable.authenticable_object is_a? Person.class
+      authenticable.authenticable_object == self
+    elsif authenticable.authenticable_object is_a? ServiceProvider.class
+      authenticable.authenticable_object.clients.include? self
+    end
+  end
+
   private
   def create_uuid
     self.uuid = SecureRandom.uuid
