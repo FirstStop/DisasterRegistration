f3cac21ea40bf25f018b75678f42abf8a90a9c2f
Andreas Thurn <AndreasThurn@gmx.de>
2014-11-29 13:32:20 +1100

    refactored split module

--- app/models/wizard/split.rb
+++ app/models/wizard/split.rb
@@ -1,18 +1,10 @@
 module Wizard
   class Split < Component
     attr_accessor :content
+    @@map = {1 => 'one', 2 => 'two', 3 => 'three'}
 
     def number
-      case @content.length
-      when 1
-        "one"
-      when 2
-         "two"
-      when 3
-        "three"
-      else
-        "unknown"
-      end
+      @@map[@content.length]
     end
   end
 end
