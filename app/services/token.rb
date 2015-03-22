b63021e791b0fb40eac1d74421923165ce2a8ee8
StephenSmithwick <stephen.smithwick@gmail.com>
2015-03-23 02:49:33 +1100

    Making the Wizard more complicated and removing hardcoded dependencies on nickname and specialty

--- app/services/token.rb
+++ app/services/token.rb
@@ -5,7 +5,7 @@ require 'prawn/measurement_extensions'
 
 module Services
     class Token
-        
+
     def self.qr_code(person, size)
         data = Rails.configuration.qr_code_generator.content(person)
         begin
@@ -16,16 +16,16 @@ module Services
             size += 1
             retry unless size > 40
         end
-    
+
     end
-    
+
     def self.generate_token_pdf(person, media)
 
         media_parameters = {
             'DC03' =>  { :width => 90, :height =>29, :leftMargin => 2, :rightMargin => 5 , :spacing => 3 },
             'DC04' =>  { :width => 90, :height =>38, :leftMargin => 1, :rightMargin => 5 , :spacing => 3 },
            'A4' =>  { :width => 90, :height =>29, :leftMargin => 2, :rightMargin => 3 , :spacing => 3 } }
-        
+
         parameters = media_parameters[media]
         Rails.logger.info "Generating token for #{person.id} #{media}"
         width = parameters[:width].send(:mm)
@@ -38,20 +38,20 @@ module Services
                                   :page_layout => :landscape,
                                   :margin => 0
                                   )
-                                  
+
         pdf.font_size = 18
         # width of paper less width of QR code less left margin less right margin less spacing
         textWidth = width - height - leftMargin - spacing - rightMargin
         # Layout is name and email on left and QR code on right with RHoK event name at bottom
-        pdf.text_box "#{person.nickname}", :at => [ leftMargin + height + spacing, height -  spacing] , :width => textWidth, :height => 20, :overflow => :shrink_to_fit
-        pdf.text_box "#{person.speciality}", :at => [leftMargin + height + spacing, height - 20  - spacing], :width => textWidth, :height => 18, :overflow => :shrink_to_fit
+        pdf.text_box "#{person.first_name}", :at => [ leftMargin + height + spacing, height -  spacing] , :width => textWidth, :height => 20, :overflow => :shrink_to_fit
+        pdf.text_box "#{person.email}", :at => [leftMargin + height + spacing, height - 20  - spacing], :width => textWidth, :height => 18, :overflow => :shrink_to_fit
 
 # pdf.text_box "#{person.super_power}", :at => [leftMargin + height + spacing, height - 20  - 16 - spacing - spacing], :width => textWidth, :height => 16, :overflow => :shrink_to_fit
         pdf.text_box "RHoK Sydney November 2014", :at => [leftMargin + height + spacing, spacing], :size => 6
         pdf.image StringIO.new(self.qr_code(person, 8).to_img.resize(300, 300).to_blob), :at => [leftMargin, height], :fit => [height, height]
         return pdf
     end
-    
+
 end
-    
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Person.create!(id: '000000000000000000000001', nickname: 'Tony', first_name: 'Tony', last_name: 'Donnaldsen', gender: 'Male', address: '14 Greentree Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW', housing_status: 'has_house', tfn: 'ABCD1234')
-Person.create!(id: '000000000000000000000002', nickname: 'Liz', first_name: 'Liz', last_name: 'Donnaldsen', gender: 'Female', address: '14 Greentree Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW', housing_status: 'has_house', tfn: '1231123')
-Person.create!(id: '000000000000000000000003', nickname: 'Jackie', first_name: 'Jackie', last_name: 'Ferus', gender: 'Female', address: '23 Yelbon Avenue', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'has_house', tfn: '098723')
-Person.create!(id: '000000000000000000000004', nickname: 'Bill', first_name: 'Bill Sigismund', last_name: 'Geyar', gender: 'Male', address: '48 Leafie Parade', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_tonight')
-Person.create!(id: '000000000000000000000005', nickname: 'Wendy', first_name: 'Wendy', last_name: 'Geyar', gender: 'Female', address: '48 Leafie Parade', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'has_house')
-Person.create!(id: '000000000000000000000006', nickname: 'Dom', first_name: 'Dom', last_name: 'Knight', gender: 'Male', address: '11 Barnstorm Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW', tfn: 'DEGK123')
-Person.create!(id: '000000000000000000000007', nickname: 'Leena', first_name: 'Leena', last_name: 'Lancasta', gender: 'Female', address: '44 Yelbon Avenue', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW')
-Person.create!(id: '000000000000000000000008', nickname: 'Richard', first_name: 'Richard James', last_name: 'Ledgend', gender: 'Male', address: '15 Timberland Close', suburb: 'Springwood', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_tonight', tfn: 'unkown')
-Person.create!(id: '000000000000000000000009', nickname: 'Beth', first_name: 'Beth', last_name: 'Ledgend', gender: 'Female', address: '15 Timberland Close', suburb: 'Springwood', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_tonight', tfn: '92132123')
-Person.create!(id: '000000000000000000000010', nickname: 'Gordon', first_name: 'Gordon Princheps', last_name: 'Murphay', gender: 'Male', address: '8 Raglin Street', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_this_week')
-Person.create!(id: '000000000000000000000011', nickname: 'John', first_name: 'John', last_name: 'Pearl', gender: 'Male', address: '42 Puncher Avenue', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'has_house')
-Person.create!(id: '000000000000000000000012', nickname: 'Carlita', first_name: 'Carlita', last_name: 'Pearl', gender: 'Female', address: '16 Munrow Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW')
-Person.create!(id: '000000000000000000000013', nickname: 'Tim', first_name: 'Tim Fanshaw', last_name: 'Pulver', gender: 'Male', address: '18 Munrow Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW')
-Person.create!(id: '000000000000000000000014', nickname: 'Wayne', first_name: 'Wayne Ian', last_name: 'Smith', gender: 'Female', address: '20 Munrow Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW', housing_status: 'has_house')
-Person.create!(id: '000000000000000000000015', nickname: 'Rose', first_name: 'Rose', last_name: 'Vine', gender: 'Female', address: '12 Raglin Street', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_this_week')
-Person.create!(id: '000000000000000000000016', nickname: 'Andrew', first_name: 'Andrew George', last_name: 'Waita', gender: 'Male', address: '31 Linklater Avenue', suburb: 'Springwood', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_this_week')
-Person.create!(id: '000000000000000000000017', nickname: 'Jill', first_name: 'Jill', last_name: 'Waita', gender: 'Female', address: '31 Linklater Avenue', suburb: 'Springwood', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_this_week')
-Person.create!(id: '000000000000000000000018', nickname: 'Seth', first_name: 'Seth', last_name: 'Walkin', gender: 'Male', address: '39 Albury Street', suburb: 'Springwood', postcode: '2777', state: 'NSW')
-Person.create!(id: '000000000000000000000019', nickname: 'Jane', first_name: 'Jane', last_name: 'Wilsun', gender: 'Female', address: '44 Redgum Close', suburb: 'Winmalee', postcode: '2777', state: 'NSW', housing_status: 'has_house')
-Person.create!(id: '000000000000000000000020', nickname: 'Irene', first_name: 'Irene', last_name: 'Yung', gender: 'Female', address: '85 Filmore Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW')
+Person.create!(id: '000000000000000000000001', email: 'Tony.Donnaldsen@gmail.com', first_name: 'Tony', last_name: 'Donnaldsen', gender: 'Male', address: '14 Greentree Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW', housing_status: 'has_house', tfn: 'ABCD1234')
+Person.create!(id: '000000000000000000000002', email: 'Liz.Donnaldsen@gmail.com', first_name: 'Liz', last_name: 'Donnaldsen', gender: 'Female', address: '14 Greentree Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW', housing_status: 'has_house', tfn: '1231123')
+Person.create!(id: '000000000000000000000003', email: 'Jackie.Ferus@gmail.com', first_name: 'Jackie', last_name: 'Ferus', gender: 'Female', address: '23 Yelbon Avenue', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'has_house', tfn: '098723')
+Person.create!(id: '000000000000000000000004', email: 'Bill.Geyar@gmail.com', first_name: 'Bill Sigismund', last_name: 'Geyar', gender: 'Male', address: '48 Leafie Parade', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_tonight')
+Person.create!(id: '000000000000000000000005', email: 'Wendy.Geyar@gmail.com', first_name: 'Wendy', last_name: 'Geyar', gender: 'Female', address: '48 Leafie Parade', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'has_house')
+Person.create!(id: '000000000000000000000006', email: 'Dom.Knight@gmail.com', first_name: 'Dom', last_name: 'Knight', gender: 'Male', address: '11 Barnstorm Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW', tfn: 'DEGK123')
+Person.create!(id: '000000000000000000000007', email: 'Leena.Lancasta@gmail.com', first_name: 'Leena', last_name: 'Lancasta', gender: 'Female', address: '44 Yelbon Avenue', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW')
+Person.create!(id: '000000000000000000000008', email: 'Richard.Ledgend@gmail.com', first_name: 'Richard James', last_name: 'Ledgend', gender: 'Male', address: '15 Timberland Close', suburb: 'Springwood', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_tonight', tfn: 'unkown')
+Person.create!(id: '000000000000000000000009', email: 'Beth.Ledgend@gmail.com', first_name: 'Beth', last_name: 'Ledgend', gender: 'Female', address: '15 Timberland Close', suburb: 'Springwood', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_tonight', tfn: '92132123')
+Person.create!(id: '000000000000000000000010', email: 'Gordon.Murphay@gmail.com', first_name: 'Gordon Princheps', last_name: 'Murphay', gender: 'Male', address: '8 Raglin Street', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_this_week')
+Person.create!(id: '000000000000000000000011', email: 'John.Pearl@gmail.com', first_name: 'John', last_name: 'Pearl', gender: 'Male', address: '42 Puncher Avenue', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'has_house')
+Person.create!(id: '000000000000000000000012', email: 'Carlita.Pearl@gmail.com', first_name: 'Carlita', last_name: 'Pearl', gender: 'Female', address: '16 Munrow Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW')
+Person.create!(id: '000000000000000000000013', email: 'Tim.Pulver@gmail.com', first_name: 'Tim Fanshaw', last_name: 'Pulver', gender: 'Male', address: '18 Munrow Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW')
+Person.create!(id: '000000000000000000000014', email: 'Wayne.Smith@gmail.com', first_name: 'Wayne Ian', last_name: 'Smith', gender: 'Female', address: '20 Munrow Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW', housing_status: 'has_house')
+Person.create!(id: '000000000000000000000015', email: 'Rose.Vine@gmail.com', first_name: 'Rose', last_name: 'Vine', gender: 'Female', address: '12 Raglin Street', suburb: 'Yellow Rock', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_this_week')
+Person.create!(id: '000000000000000000000016', email: 'Andrew.Waita@gmail.com', first_name: 'Andrew George', last_name: 'Waita', gender: 'Male', address: '31 Linklater Avenue', suburb: 'Springwood', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_this_week')
+Person.create!(id: '000000000000000000000017', email: 'Jill.Waita@gmail.com', first_name: 'Jill', last_name: 'Waita', gender: 'Female', address: '31 Linklater Avenue', suburb: 'Springwood', postcode: '2777', state: 'NSW', housing_status: 'needs_housing_this_week')
+Person.create!(id: '000000000000000000000018', email: 'Seth.Walkin@gmail.com', first_name: 'Seth', last_name: 'Walkin', gender: 'Male', address: '39 Albury Street', suburb: 'Springwood', postcode: '2777', state: 'NSW')
+Person.create!(id: '000000000000000000000019', email: 'Jane.Wilsun@gmail.com', first_name: 'Jane', last_name: 'Wilsun', gender: 'Female', address: '44 Redgum Close', suburb: 'Winmalee', postcode: '2777', state: 'NSW', housing_status: 'has_house')
+Person.create!(id: '000000000000000000000020', email: 'Irene.Yung@gmail.com', first_name: 'Irene', last_name: 'Yung', gender: 'Female', address: '85 Filmore Street', suburb: 'Winmalee', postcode: '2777', state: 'NSW')
 
 
 registration_wizard = Wizard::Wizard.new(content: [
@@ -39,9 +39,16 @@ registration_wizard = Wizard::Wizard.new(content: [
                                                                                                                               Wizard::Input.new(attribute: "first_name", label: 'First name', placeholder: ''),
                                                                                                                               Wizard::Input.new(attribute: "last_name", label: 'Last name', placeholder: '')
                                                                                                                           ]),
-                                                                                                        Wizard::Input.new(attribute: "nickname", label: 'Nickname', placeholder: ''),
+                                                                                                        Wizard::Split.new(content: [
+                                                                                                                              Wizard::NextButton.new(target: 'wizard.contact_information', label: 'Add my Email'),
+                                                                                                                              Wizard::SubmitButton.new(target: 'wizard.register', label: 'Register')
+                                                                                                                          ])
+                                                                                                    ]),
+
+                                             Wizard::WizardNode.new(name: 'wizard.contact_information', content: [
+                                                                                                        Wizard::Headline.new(text: "How do we contact you?"),
                                                                                                         Wizard::Input.new(attribute: "email", label: 'Email', placeholder: ''),
-                                                                                                        Wizard::SubmitButton.new(target: 'wizard.register', label: 'Regis')
+                                                                                                        Wizard::SubmitButton.new(target: 'wizard.register', label: 'Register')
                                                                                                     ]),
 
                                              Wizard::WizardNode.new(name: 'wizard.register', content: [
